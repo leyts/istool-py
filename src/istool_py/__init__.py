dc31e9f123f0847cd87d5552c470a1144a856510
Leyton Addison-Roach <git@laddisonroach.com>
2026-04-15 22:43:28 +0100

    refactor: remove `ArchiveMode` type from public interface

--- src/istool_py/__init__.py
+++ src/istool_py/__init__.py
@@ -18,10 +18,8 @@ from istool_py._exceptions import (
 )
 from istool_py._istool import Istool
 from istool_py._runner import CommandResult
-from istool_py.commands import ArchiveMode
 
 __all__ = [
-    "ArchiveMode",
     "CommandResult",
     "CommandValidationError",
     "Istool",
